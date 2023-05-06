0130540f8fac87698209b178c0ce47dc15af8781
Naveen Chandra <the.naveenchandra@gmail.com>
2023-05-06 12:05:28 +0530

    custom dialog box done

---
+++ lib/components/components.dart
@@ -0,0 +1,3 @@
+export './sign_in_form.dart';
+export './animated_btn.dart';
+export './custom_signin_dialog.dart';
--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
-import 'package:rive_animation/components/animated_btn.dart';
+import 'package:rive_animation/components/components.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -11,6 +11,7 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  bool isSignInDialogShown = false;
   late RiveAnimationController _btnAnimationController;
 
   @override
@@ -50,48 +51,69 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               child: const SizedBox(),
             ),
           ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Spacer(),
-                  SizedBox(
-                    width: 260,
-                    child: Column(
-                      children: const [
-                        Text(
-                          "Learn design & code.",
-                          style: TextStyle(
-                            fontSize: 60,
-                            fontFamily: "Poppins",
-                            height: 1.2,
+          AnimatedPositioned(
+            top: isSignInDialogShown ? -50 : 0,
+            duration: const Duration(milliseconds: 240),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Spacer(),
+                    SizedBox(
+                      width: 260,
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Learn design & code.",
+                            style: TextStyle(
+                              fontSize: 60,
+                              fontFamily: "Poppins",
+                              height: 1.2,
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Text(
-                          "Don't skip design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools.",
-                        ),
-                      ],
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            "Don't skip design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools.",
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const Spacer(flex: 2,),
-                  AnimatedBtn(
-                    btnAnimationController: _btnAnimationController,
-                    press: () {
-                      _btnAnimationController.isActive = true;
-                    },
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 24),
-                    child: Text(
-                      "Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, sources files and certificates.",
+                    const Spacer(
+                      flex: 2,
                     ),
-                  ),
-                ],
+                    AnimatedBtn(
+                      btnAnimationController: _btnAnimationController,
+                      press: () {
+                        _btnAnimationController.isActive = true;
+                        Future.delayed(
+                          const Duration(milliseconds: 800),
+                          () {
+                            customSigninDialog(context, onClosed: (_) {
+                              setState(() {
+                                isSignInDialogShown = false;
+                              });
+                            });
+                            setState(() {
+                              isSignInDialogShown = true;
+                            });
+                          },
+                        );
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24),
+                      child: Text(
+                        "Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, sources files and certificates.",
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
